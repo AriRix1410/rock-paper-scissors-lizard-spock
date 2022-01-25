4e086bc7152bc93a08f0e281180593809526441a
AriRix1410 <aa.fraserhaste@gmail.com>
2022-01-25 12:52:07 +0000

    Create object for outcomes of weapon selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,47 @@ let moves = 0;
 let result = document.getElementById("result");
 
 // create object for outcomes of weapon selection
+let selectedWeapon = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissors' : 'win',
+        'paper' : 'lose',
+        'lizard' : 'win',
+        'spock' : 'lose'
+    },
+
+    'scissors' : {
+        'rock' : 'lose',
+        'scissors' : 'draw',
+        'paper' : 'win',
+        'lizard' : 'lose',
+        'spock' : 'lose'
+    },
+
+    'paper' : {
+        'rock' : 'win',
+        'scissors' : 'lose',
+        'paper' : 'draw',
+        'lizard' : 'lose',
+        'spock' : 'win'
+    },
+
+    'lizard' : {
+        'rock' : 'lose',
+        'scissors' : 'lose',
+        'paper' : 'win',
+        'lizard' : 'draw',
+        'spock' : 'win'
+    },
+
+    'spock' : {
+        'rock' : 'win',
+        'scissors' : 'win',
+        'paper' : 'lose',
+        'lizard' : 'lose',
+        'spock' : 'draw'
+    },
+}
 
 // function to change moves allowed based on game difficulty on mouse click of difficulty buttons
 // if unclick then default is normal mode
