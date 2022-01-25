f05d8452e9aaeec49b4c283587ca429f628fd815
AriRix1410 <aa.fraserhaste@gmail.com>
2022-01-25 12:48:43 +0000

    Set up structure of game functions and variables

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// variables
+let computerScore = 0;
+let playerScore = 0;
+let moves = 0;
+let result = document.getElementById("result");
+
+// create object for outcomes of weapon selection
+
+// function to change moves allowed based on game difficulty on mouse click of difficulty buttons
+// if unclick then default is normal mode
+function selectDifficulty();
+
+// moves counter begins on mouse click of weapons button
+// check results of player selection vs computer generated
+function checkResult();
+
+// when counter gets to zero then winner is declared
+// game over and reload
+function gameOver();
